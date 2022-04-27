bdc34d8913cd641611985173ac3ff55725724cb8
tblack10 <travis_black@live.com>
2022-04-27 15:23:49 +1000

    Added documentation to several files, created a network call to fetch all votes of a particular vote id

---
+++ VanVotes/Screens/VoteDetailVC.swift
@@ -0,0 +1,29 @@
+//
+//  VoteDetailVC.swift
+//  VanVotes
+//
+//  Created by Travis Black on 2022-04-26.
+//
+
+import UIKit
+
+class VoteDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
